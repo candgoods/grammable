4a7960524394c100ff7f4556ab640e3089e986db
Candace <goodsoncj@gmail.com>
2017-12-03 20:01:45 +0000

    form

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,19 @@
 class GramsController < ApplicationController
   def index
   end
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = Quote.create(gram_params)
+    redirect_to root_path
+  end
+end
+
+private
+
+def gram_params
+  params.require(:gram).permit(:message)
 end
